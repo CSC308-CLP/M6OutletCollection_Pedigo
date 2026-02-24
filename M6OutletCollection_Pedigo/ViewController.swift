ba9318e618b20414932aab904ec906362a3b9b2d
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-02-24 11:48:14 -0500

    Initial Commit

---
+++ M6OutletCollection_Pedigo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M6OutletCollection_Pedigo
+//
+//  Created by Pedigo, Charles L. on 2/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
